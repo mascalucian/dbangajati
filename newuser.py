6bdbccadfdfda31542a72baf07623eb3fe72c756
Lucian Masca <lucianm0607@icloud.com>
2021-08-07 10:57:06 +0300

    Tidy: Deleted py scripts and aux files
    Facem curatenie ca asa e frumos
    Am trecut full JS

--- newuser.py
+++
@@ -1,9 +0,0 @@
-new={
-        "User ID": "4",
-        "Profile Picture":"img/lucian.jfif",
-        "First Name": "handi",
-        "Last Name": "prostea",
-        "E-mail": "aasdsa7@gmail.com",
-        "Gender": "Male",
-        "Birth Date": "12/10/1999"
-}
